f13519751458cd4abeafc359ae64a0c4494c4cf9
maria-molinal <123768478+maria-molinal@users.noreply.github.com>
2023-02-10 14:02:08 -0500

    tyfjyuvgbs

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {});
+const cardAdj = [
+  {
+    name: "melancolia",
+    img: "imagenes/melancolia.png"
+  }
+];
+
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasescogidas = [];
+var cartasescogidassId = [];
+var cartasganadas = [];
