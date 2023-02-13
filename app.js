a1744d1f75f1c9ffc66d3dacb4d8753802d7e7e8
maria-molinal <123768478+maria-molinal@users.noreply.github.com>
2023-02-13 12:34:46 -0500

    b uihj

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {});
-const cardAdj = [
-  {
-    name: "melancolia",
-    img: "imagenes/melancolia.png"
-  }
-];
+document.addEventListener("DOMContentLoaded", () => {
+  //----------------- lecture_02 ----------------------------------//
+  const cardAdj = [
+    { name: "c", img: "images/Melancolia f.jpg" },
+    { name: "css", img: "images/decepcion e.jpg" },
+    { name: "html", img: "images/feliz b.png" },
+    { name: "c", img: "images/miedo h.jpg" },
+    { name: "css", img: "images/namorado d.png" },
+    { name: "html", img: "images/rabioso c.jpg" }
+  ];
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasescogidas = [];
-var cartasescogidassId = [];
-var cartasganadas = [];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
+      // cuando i=cardAdj.length, es decir, 12.
+      var carta = document.createElement("img"); // Crea elemento img cada vez que se ejecuta la función.
+      carta.setAttribute("src", "images/reverso.png"); //Asignar nuevo atributo a cada carta
+      //igual a la imagen 'reverso'.
+      carta.setAttribute("data-id", i); //Asignar como atributo a cada carta creada, el id=i.
+      carta.addEventListener("click", voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta
+      //la función voltearCarta.
+      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
+      // cuadricula de html para que se imprima la carta creada en el div de html.
+    }
+  }
+});
