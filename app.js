6a940dc076a30f9b155aaf204e95972155a17a4d
maria-molinal <123768478+maria-molinal@users.noreply.github.com>
2023-03-10 13:54:28 -0500

    zizizizi

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     { name: "html", img: "images/feliz b.png" },
     { name: "c", img: "images/miedo h.jpg" },
     { name: "css", img: "images/namorado d.png" },
+    { name: "html", img: "images/rabioso c.jpg" },
+    { name: "c", img: "images/Melancolia f.jpg" },
+    { name: "css", img: "images/decepcion e.jpg" },
+    { name: "html", img: "images/feliz b.png" },
+    { name: "c", img: "images/miedo h.jpg" },
+    { name: "css", img: "images/namorado d.png" },
     { name: "html", img: "images/rabioso c.jpg" }
   ];
 
@@ -29,4 +35,15 @@ document.addEventListener("DOMContentLoaded", () => {
       cuadricula.appendChild(carta);
     }
   }
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
 });
